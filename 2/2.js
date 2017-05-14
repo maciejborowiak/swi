c7b440e63420467d72b547134b6dc536811046cf
Maciej Borowiak <mranonimpl@gmail.com>
2017-05-14 17:15:32 +0200

    Add files via upload

---
+++ 2/2.js
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Borowiak Code</title>
+</head>
+
+<body>
+<script>
+	$(function() {
+		$('.xop-blur-link a').hover(function() {
+			$('#xop-bg').addClass('blur');
+		},function() {
+			$('$xop-bg').removeClass('blur');
+		}
+	}
+</script>
+</body>
+</html>
